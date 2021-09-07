e6d67d033268d36e03a31ec2d971ed1a59b2ca46
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-09-07 20:33:57 +0100

    add comment.js file and add comment Dom manipulations and evenlistener

---
+++ src/modules/comment.js
@@ -0,0 +1,33 @@
+const item = []
+
+class Comment {
+  constructor(newName, newComment) {
+    this.newName = newName;
+    this.comment = newComment;
+  }
+  
+  
+}
+// let clearOut = () => {
+//   user.value = '';
+//   comment.value = '';
+// }
+
+let addComment = (user, comment) => {
+  const newComment = new Comment(user.value, comment.value);
+  item.push(newComment);
+  console.log(item);
+  // clearOut()
+  user.value ='';
+  comment.value = '';
+}
+  
+
+document.querySelector('#submit').addEventListener('click', (e) => {
+  e.preventDefault();
+  let user = document.querySelector('#form-popup').querySelector('input[name="input-name"]')
+  let comment = document.querySelector('#form-popup').querySelector('#input')
+  addComment(user, comment)
+})
+
+export default item;
