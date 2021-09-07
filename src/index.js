edaf4fe8fe06a94c28350e8dd1c81171f68576ef
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-09-07 20:32:54 +0100

    import comment.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import retrieveData from './modules/retrieveData';
 import setData from './modules/setData.js';
 import createBasket from './modules/createBasket.js';
 import Utilities from './modules/utilities.js';
+import item from './modules/comment.js';
+
 
 const apiBasket = 'https://getpantry.cloud/apiv1/pantry/f30333b4-125d-4db2-8a42-2c216adec5bc/basket/games'
 window.addEventListener('DOMContentLoaded', () => {''
