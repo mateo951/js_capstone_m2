511ff587b8bcc46872aa0ef270036eaf25a6b17f
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-09-10 04:10:27 -0500

    Add linters fixes v2

--- src/modules/showLikes.js
+++ src/modules/showLikes.js
@@ -4,8 +4,8 @@ const showLikes = async (likes) => {
   const jokes = Jokes.getJokes();
   Object.values(jokes).forEach((joke) => {
     likes.forEach((likedItem) => {
-      const span = document.getElementById(joke.id);
-      if (joke.id === likedItem.item_id) {
+      const span = document.getElementById(joke.id); // eslint-disable-next-line
+      if (joke.id == likedItem.item_id) {
         span.innerHTML = likedItem.likes;
       }
     });
